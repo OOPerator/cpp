46c3b159363f587de007a2cd929caa7e9874ea27
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-07-18 06:54:48 -0600

    Update Rng.cpp

--- Rng.cpp
+++ Rng.cpp
@@ -5,15 +5,10 @@
 int main ()
 {
     srand(time(NULL));
-    int digits = 6;
-    std::vector<int> table = {};
-    for (int i=0;i<digits;i++)
+    std::vector<char>table{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','r','x','y','z'};
+    for (int i=0;i<table.size();i++)
     {
-        table.push_back(rand()%10);
-    }
-    for(int i=0;i<table.size();i++)
-    {
-        std::cout<<table[i];
+      std::cout<<table[rand()%table.size()];
     }
     return 0;
 }
