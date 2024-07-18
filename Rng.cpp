259b804b9904213d48d51ccc02a859cbc80056b1
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-07-18 10:34:40 -0600

    Update Rng.cpp

--- Rng.cpp
+++ Rng.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <cstdlib>
 
 int main ()
 {
-    srand(time(NULL));
-    std::vector<char>table{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','r','x','y','z'};
-    for (int i=0;i<table.size();i++)
-    {
-      std::cout<<table[rand()%table.size()];
-    }
-    return 0;
+   srand(time(NULL));
+   std::string chars = "abcdefghijklmnopqrstuvwrxyz";
+   int len = chars.length();
+   for(int i=0;i<len;i++)
+   {
+       std::cout<<chars[rand()%len];
+   }
 }
