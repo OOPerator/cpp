ceb7377dcf56b288655eae7e32bcea26660d1729
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-07-23 00:43:25 -0600

    Update Rng.cpp

--- Rng.cpp
+++ Rng.cpp
@@ -4,12 +4,12 @@
 #include <algorithm>
 int main()
 {
-    std::string table{"0123456789"};
+    std::string table{"01234567890123456789"};
     std::random_device rnd;
     std::mt19937 gen(rnd());
     std::shuffle(table.begin(),table.end(),gen);
-    int num_chars = 4;
-    for(int i=0;i<num_chars;i++)
+    int clamp = table.length();
+    for(int i=0;i<clamp;i++)
     {
         std::cout<<table[i];
     }
