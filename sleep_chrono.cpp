55e2302ce7d8bddb0095981b431c16226765547a
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-03 16:14:32 -0600

    Update sleep_chrono.cpp

--- sleep_chrono.cpp
+++ sleep_chrono.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-
 void sleep(int ms)
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
-
 int main()
 {
     int count = 1;
