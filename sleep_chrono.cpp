0b5890578d33911540f7482d4e393da24e948c86
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-03 16:14:04 -0600

    Create sleep_chrono.cpp

---
+++ sleep_chrono.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void sleep(int ms)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+int main()
+{
+    int count = 1;
+    do
+    {
+        sleep(100);
+        std::cout<<count++<<"\n";
+    }
+    while(count<21);
+    return 0;
+}
