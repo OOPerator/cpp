1646e4d8121957fce231d0f7cbfc053ad3037dfc
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-04 01:50:08 -0600

    Update sleep_chrono.cpp

--- sleep_chrono.cpp
+++ sleep_chrono.cpp
@@ -3,10 +3,15 @@
 #include <chrono>
 void sleep(int ms)
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    using namespace std;
+    this_thread::sleep_for(chrono::milliseconds(ms));
 }
 int main()
 {
-    for (int i=0;i<21;i++) {sleep(300); std::cout<<i<<"\r";}
+    int i = 1;
+    while(i<11)
+    {
+        sleep(300);std::cout<<i++<<"\n";
+    }
     return 0;
 }
