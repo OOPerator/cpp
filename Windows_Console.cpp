b6246b3d1d2eb354dc77d4e2e41bdc13d0f81d9d
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-06 23:06:16 -0600

    Create windows_console_test.cpp

---
+++ Windows_Console.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cwchar>
+#include <windows.h>
+#include <thread>
+#include <chrono>
+void sleep(int ms)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+void set_font()
+{
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;
+    cfi.dwFontSize.Y = 30;
+    cfi.FontFamily = FF_DONTCARE;        //is it really called DONTCARE?
+    cfi.FontWeight = FW_NORMAL;
+    std::wcscpy(cfi.FaceName, L"Consolas");
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+    system("Color 0A");
+}
+int main()
+{
+    set_font();
+    std::string text0 = "Hello, this is a test program to print letters with a delay.\nPress the exit button to close.............";
+    while(true)
+    {
+        for(char i:text0)
+        {
+            sleep(150);
+            std::cout<<i;
+            std::cout.clear();
+            std::cout.flush();
+        }
+        system("cls");
+    }
+return 0;
+}
