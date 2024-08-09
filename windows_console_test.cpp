179a4f0bf96f4d74054886aacdee62ed108b0f9e
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-08 22:45:09 -0600

    Update windows_console_test.cpp

--- windows_console_test.cpp
+++ windows_console_test.cpp
@@ -8,14 +8,14 @@ void sleep(int ms)
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
-void set_font_win32_console()
+void setfont_win32console()
 {
     CONSOLE_FONT_INFOEX cfi;
     cfi.cbSize = sizeof(cfi);
     cfi.nFont = 0;
     cfi.dwFontSize.X = 0;
     cfi.dwFontSize.Y = 30;
-    cfi.FontFamily = FF_DONTCARE;        //is it really called DONTCARE? I actually care though wtf.
+    cfi.FontFamily = FF_DONTCARE;
     cfi.FontWeight = FW_NORMAL;
     std::wcscpy(cfi.FaceName, L"Consolas");
     SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
@@ -23,8 +23,8 @@ void set_font_win32_console()
 }
 int main()
 {
-    set_font_win32_console();
-    std::string text0 = "Hello, this is a test program to print letters with a delay...........";
+    setfont_win32console();
+    std::string text0 = "Hello, this is a test program to print letters one by one...........";
     while(true)
     {
         for(char i:text0)
