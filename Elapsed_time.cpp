dae144ac9cbf34f61d0c1aa0aae8ebcbf7c34080
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-14 21:53:04 -0600

    Update Elapsed_time.cpp

--- Elapsed_time.cpp
+++ Elapsed_time.cpp
@@ -4,8 +4,8 @@ int main()
 {
     auto start = std::chrono::high_resolution_clock::now();
     int n = 0;
-    while(n<1000000000){n++;}
+    while(n<INT_MAX){n++;}
     auto ended = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = ended-start;
-    std::cout<<elapsed.count();
+    std::cout<<n<<"\n"<<elapsed.count();
 }
