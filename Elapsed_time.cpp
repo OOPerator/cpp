9de7a3a3a48e5001ab78b81140cfcdbbc0d68d20
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2024-08-16 22:47:05 -0600

    Update Elapsed_time.cpp

--- Elapsed_time.cpp
+++ Elapsed_time.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
+void sleep(int ms)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
 int main()
 {
     auto start = std::chrono::high_resolution_clock::now();
-    int n = 0;
-    while(n<INT_MAX){n++;}
-    auto ended = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = ended-start;
-    std::cout<<n<<"\n"<<elapsed.count();
+    int n = 1;
+    while(n<11)
+    {
+        std::cout<<n++<<"\n";
+        sleep(1000);
+    }
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish-start;
+    std::cout<<"time elapsed: "<<elapsed.count()<<" seconds";
 }
