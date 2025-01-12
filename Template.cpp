7b7d9ab5a10f38de629a6e63e43851dbc3ec184e
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2025-01-11 23:41:41 -0700

    Update Template.cpp
    Better example

--- Template.cpp
+++ Template.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
-template<typename T>
-void swap(T &a, T &b)
-{
-    T temp = a;
-    a = b;
-    b = temp;
-}
+using namespace std;
+
+template<typename N>
+void swap_out(N &value0, N &value1)
+   {
+        N placeholder = value0;
+        value0 = value1;
+        value1 = placeholder;
+   }
+    
 int main()
 {
-    double a = 10;
-    double b = 20;
-    swap(a,b);
-    std::cout<<a<<"\n"<<b;
+    int a(20),b(10);
+    swap_out(a,b);
+    cout<<a<<" "<<b<<"\n";
+    std::string x("I am"),y("Yoda");
+    swap_out(x,y);
+    cout<<x<<" "<<y<<"\n";
+    return 0;
 }
