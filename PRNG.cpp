66341b3f8560c72324fa3b71d4b856970eede824
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2025-07-04 11:10:41 -0600

    Update PRNG.cpp

--- PRNG.cpp
+++ PRNG.cpp
@@ -2,12 +2,12 @@
 #include <random>
 #include <cstdint>
 using std::cout;
-int64_t getrand(const int64_t &A, const int64_t &B)
+int64_t getrand(const int64_t &begin, const int64_t &end)
 {
-    static std::random_device rd;
-    static std::mt19937 twister(rd());
+    static std::random_device RD;
+    static std::mt19937 twister(RD());
     static std::uniform_int_distribution<int64_t> dist;
-    dist.param(std::uniform_int_distribution<int64_t>::param_type(A,B));
+    dist.param(std::uniform_int_distribution<int64_t>::param_type(begin,end));
     return dist(twister);
 }
 
@@ -17,5 +17,6 @@ int main()
   cout<<getrand(INT16_MIN,INT16_MAX)<<"\n";
   cout<<getrand(INT32_MIN,INT32_MAX)<<"\n";
   cout<<getrand(INT64_MIN,INT64_MAX)<<"\n";
+  cout<<"dice roll: you landed a "<<getrand(1,6);
   return 0;
 }
