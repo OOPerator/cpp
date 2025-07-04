58b1b154e0c97fd2c17b2546fcbe70f3d5ab25fb
ShowerShark <109652943+OOPerator@users.noreply.github.com>
2025-07-04 10:15:25 -0600

    Update Rng.cpp

--- Rng.cpp
+++ Rng.cpp
@@ -6,9 +6,7 @@ int main()
 {
    unsigned seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
    std::mt19937 rng(seed);
-   std::uniform_int_distribution<size_t> dist(0,UINT32_MAX);
-   
-   std::cout<<dist(rng);
-   
+   std::uniform_int_distribution<int64_t> randomint64(INT64_MIN,INT64_MAX);
+   std::cout<<randomint64(rng)<<"\n";
   return 0;
 }
